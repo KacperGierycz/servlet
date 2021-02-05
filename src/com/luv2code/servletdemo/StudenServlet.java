ef5935cf12d5c111dec45ad8a578a1f56f1a0878
Kacper <kacpergierycz@gmail.com>
2021-02-05 17:09:01 +0100

    reading data from html with servlet request.getParameter

--- src/com/luv2code/servletdemo/StudenServlet.java
+++ src/com/luv2code/servletdemo/StudenServlet.java
@@ -10,16 +10,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class HelloWorldServlet
+ * Servlet implementation class StudenServlet
  */
-@WebServlet("/HelloWorldServlet")
-public class HelloWorldServlet extends HttpServlet {
+@WebServlet("/StudenServlet")
+public class StudenServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public HelloWorldServlet() {
+    public StudenServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -28,28 +28,24 @@ public class HelloWorldServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		// Step 1: set the content type
+
+		// Step 1: set content type
 		response.setContentType("text/html");
 		
 		// Step 2: get the printwriter
-		PrintWriter out = response.getWriter();
+		PrintWriter out=response.getWriter();
 		
-		// Step 3: generate HTML content
+		// Step 3: generate the html content
 		out.println("<html><body>");
 		
-		out.println("<h2>Hello</h2>");
-		
-		out.println("<hr>");
-		out.println("Time on the server is:"+ new java.util.Date()
-				);
+		out.println("The student is Confirmed: "
+				+request.getParameter("firstName")+
+				" "+ request.getParameter("lastName"));
 		
 		out.println("</body></html>");
-		
-		
 	}
 
-	
+		
 	
 	
 	
